896415f10799b2861333bb6656472aa91eab6f35
Danielle Burgess <develleoper@users.noreply.github.com>
2021-01-23 16:15:46 -0800

    Initial WIP progress on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,87 @@
+import { Map, TileLayer, Marker, Popup, Tooltip } from "react-leaflet";
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 import './Map.css'
+import greenLMarker from '../resources/green-marker.png'
+import blueLMarker from '../resources/blue-marker.png'
 
+/* TEMP MOCK VALUES */
+const updateListing = () => {}
+const selectedListing = null
+let markers = []
+/* /TEMP */
+
+
+/* TEMP COPIED FROM EXAMPLE */
+function Markers (props) {
+  const { mapData, updateListing, selectedListing } = props;
+  const bindMarker = (ref) => {
+    if (ref) {
+      const marker = ref.leafletElement;
+      markers.push(marker);
+    }
+  }
+
+  return (
+    <MarkerClusterGroup>
+      {mapData.map((item, index) =>
+        <Marker
+          key={`${item.popup.id}-${index}`}
+          ref={bindMarker}
+          position={item.coords}
+          id={item.popup.id}
+          icon={
+            selectedListing === item.popup.id ? greenLMarker : blueLMarker
+          }
+        >
+          <Popup>
+            <div className="popup-container">
+              <div>{item.popup.listing}</div>
+              <div>{`${item.popup.street} ${item.popup.street2}`}</div>
+              <div
+                className="popup-show-details"
+                onClick={() => {
+                  updateListing(item.popup.id, "popup");
+                }}
+              >
+                Show Details
+              </div>
+            </div>
+          </Popup>
+            <Tooltip>
+              <div className="popup-tooltip">
+                <div>{item.popup.listing}</div>
+              </div>
+            </Tooltip>
+        </Marker>
+      )}
+    </MarkerClusterGroup>
+  )
+}
+/* /TEMP */
 
 function MapPage({mapData}) {
   return (<>
     <p>{JSON.stringify(mapData)}</p>
+    <Map
+      center={[45.52345, -122.6762]}
+      zoom={10}
+      minZoom={8}
+      maxZoom={18}
+      scrollWheelZoom={true}
+      tap={true}
+      dragging={true}
+      touchZoom={true}
+    >
+      <TileLayer
+        attribution=""
+        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      />
+      <Markers
+        mapData={mapData}
+        updateListing={updateListing}
+        selectedListing={selectedListing}
+      />
+    </Map>
   </>)
 }
 
