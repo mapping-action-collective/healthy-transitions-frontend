b2579fd7d5ab32c194a7cb8ced4a6671cfc7c69f
Winter Runion <49598669+wrunion@users.noreply.github.com>
2021-12-16 09:06:57 -0800

    Change title text, fix spelling
    With an eye to our current round of funding applications, we need the upper left text to say, "Healthy Transitions" instead of "Mapping Action Collective." This may be a temporary or permanent change, but needs to stay in place until at least Jan 7, 2022.

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,10 +8,10 @@ function Page({children}) {
   return (<>
     <Segment as="header" basic vertical inverted>
       <Menu as="nav" size="massive" color="teal" secondary pointing className="container">
-        <Menu.Item as={NavLink} to="/" end className="mac-logo">Mapping Action Collective</Menu.Item>
+        <Menu.Item as={NavLink} to="/" end className="mac-logo">Healthy Transitions</Menu.Item>
         <Menu.Item as={NavLink} to="/" end>Oregon Youth Resource Map</Menu.Item>
         <Menu.Item as={NavLink} to="/about" position="right"><header>About</header></Menu.Item>
-        <Menu.Item as={NavLink} to="/suggest"><header>Suggest Upate</header></Menu.Item>
+        <Menu.Item as={NavLink} to="/suggest"><header>Suggest Update</header></Menu.Item>
       </Menu>
     </Segment>
     {children}
