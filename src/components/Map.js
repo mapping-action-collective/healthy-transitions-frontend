54272714d172e0031e4ffdae24d0267506789be2
Danielle Burgess <develleoper@users.noreply.github.com>
2021-12-28 13:31:54 -0800

    update styles, mobile support, card scrolling snap, and map scrolling navigation

--- src/components/Map.js
+++ src/components/Map.js
@@ -24,12 +24,13 @@ function MapPage({ listings }) {
   }, {})
   const filteredListings = filterListings(listings, searchParams, search)
   const cardRefs = listings.reduce((cardRefs, listing) => ({...cardRefs, [listing.guid]: createRef()}), {})
+  const mapRef = createRef()
 
   return (<>
     <MapNavigation categories={categories} search={search} setSearch={setSearch} />
     <Container as="main" id="map-page">
-      <MapCards listings={filteredListings} cardRefs={cardRefs} />
-      <MapMap listings={filteredListings} cardRefs={cardRefs} />
+      <MapCards listings={filteredListings} cardRefs={cardRefs} mapRef={mapRef} />
+      <MapMap listings={filteredListings} cardRefs={cardRefs} ref={mapRef} />
     </Container>
   </>)
 }
@@ -63,87 +64,91 @@ function MapNavigation({ categories, search, setSearch }) {
   </>)
 }
 
-function MapCards({ listings, cardRefs }) {
+function MapCards({ listings, cardRefs, mapRef }) {
   const { markerId } = useParams()
-  const [ searchParams, setSearchParams ] = useSearchParams()
   const currentCard = cardRefs[markerId]
   useEffect(() => currentCard && currentCard.current?.scrollIntoView({behavior: "smooth"}), [currentCard])
   return (
     <Card.Group as="section" itemsPerRow="1">
-      {listings.map((listing, index) => <MapCard key={listing.guid} listing={listing} index={index} ref={cardRefs[listing.guid]} searchParams={searchParams} setSearchParams={setSearchParams} />)}
+      {listings.map((listing, index) => <MapCard key={listing.guid} listing={listing} index={index} ref={cardRefs[listing.guid]} mapRef={mapRef} />)}
     </Card.Group>
   )
 }
 
-const CardCornerDropdown = ({ index, guid, full_address='' }) => {
+const CardCornerDropdown = ({ index, guid, full_address='', mapRef }) => {
+  const [ searchParams, ] = useSearchParams()
+  const navigate = useNavigate()
   return (
-    <Dropdown icon='angle down' direction='left'>
-    <Dropdown.Menu>
+    <Dropdown icon='angle down' direction='left' onFocus={() => navigate(`/${guid}?${searchParams.toString()}`)}>
+      <Dropdown.Menu>
 
-      {/* Proposed optonal UI element for saving listings */}
-      {/* <Dropdown.Item id={guid} text='Save listing' icon={{ name: 'heart outline', color: getColor(index)}} onClick={() => console.log(guid)} /> */}
+        {/* Proposed optonal UI element for saving listings */}
+        {/* <Dropdown.Item id={guid} text='Save listing' icon={{ name: 'heart outline', color: getColor(index)}} onClick={() => console.log(guid)} /> */}
 
-      {/* This copies the URL of the card to the user's clipboard. 
-      - TODO: add a UI element that conveys "Link copied!" on success 
-      - TODO: investigate accessibility of Semantic UI dropdowns.
-      */}
-      <Dropdown.Item text='Copy link'icon='share alternate'
-      onClick={() => navigator.clipboard.writeText(`oregonyouthresourcemap.com/#/${guid}`)}
-      />
+        {/* This copies the URL of the card to the user's clipboard. 
+        - TODO: add a UI element that conveys "Link copied!" on success 
+        - TODO: investigate accessibility of Semantic UI dropdowns.
+        */}
+        <Dropdown.Item text='Copy link'icon='share alternate'
+        onClick={() => navigator.clipboard.writeText(`oregonyouthresourcemap.com/#/${guid}`)}
+        />
 
-      {/* Links to map when applicable. Does not display on cards with no address.  */}
-      {full_address && <Dropdown.Item as={Link} to={`/${guid}`} text='View on map' icon={{ name: 'map outline', color: getColor(index)}}/>}
+        {/* Links to map when applicable. Does not display on cards with no address.  */}
+        {full_address && <Dropdown.Item onClick={() => { mapRef.current?.scrollIntoView({ behavior: 'smooth' }) }} text='View on map' icon={{ name: 'map outline', color: getColor(index)}}/>}
 
-      {/* External link to Google feedback form, as requested by HT youth  */}
-      <Dropdown.Item as={'a'} href='https://forms.gle/Ldo4ortzkNHDxSGB8' target='_blank' text='Comment'icon={{ name: 'chat outline', color: getColor(index)}} />
-    </Dropdown.Menu>
-  </Dropdown>
+        {/* External link to Google feedback form, as requested by HT youth  */}
+        <Dropdown.Item as={'a'} href='https://forms.gle/Ldo4ortzkNHDxSGB8' target='_blank' text='Comment'icon={{ name: 'chat outline', color: getColor(index)}} />
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
 
-const MapCard = forwardRef(({ searchParams, setSearchParams, listing: { guid, category, coords, parent_organization, full_name, full_address, description, phone_1, phone_label_1, phone_2, phone_label_2, crisis_line_number, crisis_line_label, website, blog_link, twitter_link, facebook_link, youtube_link, instagram_link, video_description, languages_offered, services_provided, keywords, min_age, max_age, eligibility_requirements, ...listing}, index}, ref) =>
-  <Ref innerRef={ref}>
-    <Card as="article" color={getColor(index)} centered raised className="map-card">
-      <Card.Content>
-        <div style={{ display: "flex", justifyContent: 'space-between'}}>
-          <Label as={Link} to={parent_organization ? `/?parent_organization=${encodeURIComponent(parent_organization)}` : `/?full_name=${encodeURIComponent(full_name)}`} ribbon color={getColor(index)} style={{marginBottom: `1em`}}>{parent_organization || full_name}</Label>
-          {CardCornerDropdown({index, guid, full_address})}
-          </div>
-        <Card.Header><Link to={`/${guid}`}>{full_name}</Link></Card.Header>
-        <Card.Meta><Link to={`/${guid}`}>{full_address}</Link></Card.Meta>
-        <Segment secondary>
-          { full_address && <Card.Description><Icon name="map marker alternate" /><a target="_blank" rel="noreferrer" href={`https://www.google.com/maps/dir//${encodeURIComponent(full_address)}`}>Get Directions <sup><Icon size="small" name="external" /></sup></a></Card.Description> }
-          { phone_1 && <Card.Description><Icon name="phone" />{phone_label_1}: <a target="_blank" rel="noreferrer" href={`tel:${phone_1}`}>{phone_1}</a></Card.Description> }
-          { phone_2 && <Card.Description><Icon name="phone" />{phone_label_2}: <a target="_blank" rel="noreferrer" href={`tel:${phone_2}`}>{phone_2}</a></Card.Description> }
-          { crisis_line_number && <Card.Description><Icon name="phone" />{crisis_line_label}: <a target="_blank" rel="noreferrer" href={`tel:${crisis_line_number}`}>{crisis_line_number}</a></Card.Description> }
-          { website && <Card.Description><Icon name="globe" /><a target="_blank" rel="noreferrer" href={website}>Website</a></Card.Description> }
-          { blog_link && <Card.Description><Icon name="globe" /><a target="_blank" rel="noreferrer" href={blog_link}>{blog_link}</a></Card.Description> }
-          { twitter_link && <Card.Description><Icon name="twitter" /><a target="_blank" rel="noreferrer" href={twitter_link}>{twitter_link}</a></Card.Description> }
-          { facebook_link && <Card.Description><Icon name="facebook" /><a target="_blank" rel="noreferrer" href={facebook_link}>{facebook_link}</a></Card.Description> }
-          { youtube_link && <Card.Description><Icon name="youtube" /><a target="_blank" rel="noreferrer" href={youtube_link}>{youtube_link}</a></Card.Description> }
-          { instagram_link && <Card.Description><Icon name="instagram" /><a target="_blank" rel="noreferrer" href={instagram_link}>{instagram_link}</a></Card.Description> }
-        </Segment>
-        <Segment basic vertical>
-          <ExpandableDescription label="Description" value={description} />
-        </Segment>
-        <Segment basic vertical>
-          { video_description && <Card.Description>Video description: <a href={video_description}>{video_description}</a></Card.Description> }
-        </Segment>
-        <Segment secondary>
-          { (min_age || max_age) && <Card.Description><Card.Header as="strong">Age:</Card.Header> {[ min_age, max_age ].join(`-`)}</Card.Description> }
-          { eligibility_requirements && <Card.Description><Card.Header as="strong">Eligibility Requirements:</Card.Header> {eligibility_requirements}</Card.Description> }
-          <ValueList name="Languages Offered" values={languages_offered} />
-          <ValueList name="Services" values={services_provided} />
-        </Segment>
-        {/* <Card.Description as="dl">{Object.entries(listing).filter(([dt, dd]) => dd).map(([dt, dd], i) => <><dt key={dt}>{dt}</dt><dd key={i}>{dd}</dd></>)}</Card.Description> */}
-      </Card.Content>
-      <Card.Content extra>
-        <NavLink to={`/?category=${encodeURIComponent(category)}`}># {category.split(':')[1]}</NavLink>
-        { keywords && keywords.map((keyword, i) => <NavLink to={`/?${new URLSearchParams({...Object.fromEntries(searchParams), tag: `${keyword}` }).toString()}`} key={keyword} onClick={() => setSearchParams({ ...Object.fromEntries(searchParams), tag: keyword })}>&nbsp; &nbsp;# {keyword}</NavLink> )}
-      </Card.Content>
-    </Card>
-  </Ref>
-)
+const MapCard = forwardRef(({ mapRef, listing: { guid, category, coords, parent_organization, full_name, full_address, description, phone_1, phone_label_1, phone_2, phone_label_2, crisis_line_number, crisis_line_label, website, blog_link, twitter_link, facebook_link, youtube_link, instagram_link, video_description, languages_offered, services_provided, keywords, min_age, max_age, eligibility_requirements, ...listing}, index}, ref) => {
+  const [ searchParams, setSearchParams ] = useSearchParams()
+  return (
+    <Ref innerRef={ref}>
+      <Card as="article" color={getColor(index)} centered raised className="map-card">
+        <Card.Content>
+          <div style={{ display: "flex", justifyContent: 'space-between'}}>
+            <Label as={Link} to={parent_organization ? `/?parent_organization=${encodeURIComponent(parent_organization)}` : `/?full_name=${encodeURIComponent(full_name)}`} ribbon color={getColor(index)} style={{marginBottom: `1em`}}>{parent_organization || full_name}</Label>
+            {CardCornerDropdown({index, guid, full_address, mapRef})}
+            </div>
+          <Card.Header><Link to={`/${guid}`}>{full_name}</Link></Card.Header>
+          <Card.Meta><Link to={`/${guid}`}>{full_address}</Link></Card.Meta>
+          <Segment secondary>
+            { full_address && <Card.Description><Icon name="map marker alternate" /><a target="_blank" rel="noreferrer" href={`https://www.google.com/maps/dir//${encodeURIComponent(full_address)}`}>Get Directions <sup><Icon size="small" name="external" /></sup></a></Card.Description> }
+            { phone_1 && <Card.Description><Icon name="phone" />{phone_label_1}: <a target="_blank" rel="noreferrer" href={`tel:${phone_1}`}>{phone_1}</a></Card.Description> }
+            { phone_2 && <Card.Description><Icon name="phone" />{phone_label_2}: <a target="_blank" rel="noreferrer" href={`tel:${phone_2}`}>{phone_2}</a></Card.Description> }
+            { crisis_line_number && <Card.Description><Icon name="phone" />{crisis_line_label}: <a target="_blank" rel="noreferrer" href={`tel:${crisis_line_number}`}>{crisis_line_number}</a></Card.Description> }
+            { website && <Card.Description><Icon name="globe" /><a target="_blank" rel="noreferrer" href={website}>Website</a></Card.Description> }
+            { blog_link && <Card.Description><Icon name="globe" /><a target="_blank" rel="noreferrer" href={blog_link}>{blog_link}</a></Card.Description> }
+            { twitter_link && <Card.Description><Icon name="twitter" /><a target="_blank" rel="noreferrer" href={twitter_link}>{twitter_link}</a></Card.Description> }
+            { facebook_link && <Card.Description><Icon name="facebook" /><a target="_blank" rel="noreferrer" href={facebook_link}>{facebook_link}</a></Card.Description> }
+            { youtube_link && <Card.Description><Icon name="youtube" /><a target="_blank" rel="noreferrer" href={youtube_link}>{youtube_link}</a></Card.Description> }
+            { instagram_link && <Card.Description><Icon name="instagram" /><a target="_blank" rel="noreferrer" href={instagram_link}>{instagram_link}</a></Card.Description> }
+          </Segment>
+          <Segment basic vertical>
+            <ExpandableDescription label="Description" value={description} />
+          </Segment>
+          <Segment basic vertical>
+            { video_description && <Card.Description>Video description: <a href={video_description}>{video_description}</a></Card.Description> }
+          </Segment>
+          <Segment secondary>
+            { (min_age || max_age) && <Card.Description><Card.Header as="strong">Age:</Card.Header> {[ min_age, max_age ].join(`-`)}</Card.Description> }
+            { eligibility_requirements && <Card.Description><Card.Header as="strong">Eligibility Requirements:</Card.Header> {eligibility_requirements}</Card.Description> }
+            <ValueList name="Languages Offered" values={languages_offered} />
+            <ValueList name="Services" values={services_provided} />
+          </Segment>
+          {/* <Card.Description as="dl">{Object.entries(listing).filter(([dt, dd]) => dd).map(([dt, dd], i) => <><dt key={dt}>{dt}</dt><dd key={i}>{dd}</dd></>)}</Card.Description> */}
+        </Card.Content>
+        <Card.Content extra>
+          <NavLink to={`/?category=${encodeURIComponent(category)}`}># {category.split(':')[1]}</NavLink>
+          { keywords && keywords.map((keyword, i) => <NavLink to={`/?${new URLSearchParams({...Object.fromEntries(searchParams), tag: `${keyword}` }).toString()}`} key={keyword} onClick={() => setSearchParams({ ...Object.fromEntries(searchParams), tag: keyword })}>&nbsp; &nbsp;# {keyword}</NavLink> )}
+        </Card.Content>
+      </Card>
+    </Ref>
+  )
+})
 
 const ExpandableDescription = ({ label, value }) => <>
     { label && <Card.Header as="strong">{label}:</Card.Header> }
@@ -157,14 +162,16 @@ const ValueList = ({ name, values }) => values && (
   </List>
 )
 
-function MapMap({ listings, cardRefs }) {
+const MapMap = forwardRef(({ listings, cardRefs }, ref) => {
   return (
-    <Segment as={MapContainer} center={[44.0489388,-123.0919415]} zoom={10} minZoom={8} maxZoom={18} scrollWheelZoom={false} tap={true} dragging={true} touchZoom={true}>
-      <TileLayer attribution="" url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png" />
-      <MapMarkers listings={listings} cardRefs={cardRefs} />
-    </Segment>
+    <Ref innerRef={ref}>
+      <Segment as={MapContainer} center={[44.0489388,-123.0919415]} zoom={10} minZoom={8} maxZoom={18} scrollWheelZoom={false} tap={true} dragging={true} touchZoom={true}>
+        <TileLayer attribution="" url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png" />
+        <MapMarkers listings={listings} cardRefs={cardRefs} />
+      </Segment>
+    </Ref>
   )
-}
+})
 
 function MapMarkers ({listings, cardRefs}) {
   const { markerId } = useParams()
