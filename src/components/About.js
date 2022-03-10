63461bc28082b8ce208c354d8e16e926f46444c2
wrunion <winterrunion@gmail.com>
2022-03-09 17:33:29 -0800

    Format contributor logo display.

--- src/components/About.js
+++ src/components/About.js
@@ -16,6 +16,9 @@ function AboutPage({aboutText, contributors, disclaimer, videoLink, rubric}) {
 
   const whiteBirdAttribution = (<Item style={whiteBirdStyle}><Item.Image size='tiny' src={whiteBirdLogo} /><Item.Content style={{alignSelf: "center"}}>Some data was sourced from The Little Help Book published by White Bird Clinic.</Item.Content></Item>)
 
+  const genLogoStyle = { maxWidth: '125px', maxHeight: '75px', marginRight: '1.5em'}
+  const dsLogoStyle = { width: '150px', height: '75px', marginRight: '1em' }
+
   return (
     <Container as="main" id="about-page" text>
       <Segment as="article" basic vertical>
@@ -24,13 +27,14 @@ function AboutPage({aboutText, contributors, disclaimer, videoLink, rubric}) {
         {/* DO NOT REMOVE. Required by White Bird TOS. */}
         {whiteBirdAttribution}
         {/* TODO: add segment or similar to display rubric text here */}
-        {dsLogoDisplay}
+        {/* {dsLogoDisplay} */}
         </Segment>
         {/* List of contributors/sponsors, including MAC */}
         {contributors &&<Segment as="article" vertical>
           <Header as="h1">About Our Contributors</Header>
           {contributors.map((entry, index) => (
             <Item style={whiteBirdStyle} key={entry.name}>
+              {entry.logo && <Image src={entry.logo} align='center' style={entry.name === 'Direction Service' ? dsLogoStyle : genLogoStyle} />}
               <Item.Content>
                 <Header as="a" color={getColor(index)} style={contributorStyle} href={entry.website_url ?? null} target="_blank">{entry.name}</Header>
                 {entry.description && <Item.Description style={greyText}>{entry.description}</Item.Description>}
